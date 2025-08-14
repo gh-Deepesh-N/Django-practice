795ef1da52b27b542cc6dfe9d39225f2380da173
Deepesh N <ndeepesh2004@gmail.com>
2025-08-14 22:10:13 +0530

    learning

--- first/travello/views.py
+++ first/travello/views.py
@@ -3,13 +3,20 @@ from .models import Destination
 # Create your views here.
 def index(request):
     dest1 = Destination() 
+    dest2 = Destination()
     dest1.name = 'Mumbai'
     dest1.desc = 'The City That Never Sleeps'
     dest1.price = 700
     dest1.img = 'destination_1.jpg'
     dest1.offer = True
 
-    dests = [dest1]
+    dest2.name = 'eiffel tower(paris)'
+    dest2.desc = 'The City of Light'
+    dest2.price = 1000
+    dest2.img = 'destination_2.jpg'
+    dest2.offer = True
+
+    dests = [dest1, dest2]
     return render(request, "index.html", {'dests': dests})
     
 
